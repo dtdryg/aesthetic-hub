78e3a3ff2a8bf867ada2345c5f201ea7543745a5
dtdryg <bo3andotherstuff11@gmail.com>
2026-03-14 17:58:15 +0400

    Update ipfs.js

--- src/ipfs.js
+++ src/ipfs.js
@@ -1,24 +1,13 @@
-import { Web3Storage } from "web3.storage";
-
 export function getIPFSGateway(cidOrUri) {
   return `https://ipfs.io/ipfs/${cidOrUri.replace('ipfs://','')}`;
 }
 
-function getClient(token) {
-  if (!token) throw new Error("Missing VITE_WEB3_STORAGE_TOKEN");
-  return new Web3Storage({ token });
-}
-
 export async function putFile(file, token) {
-  const client = getClient(token);
-  const cid = await client.put([file], { wrapWithDirectory: false });
-  return `ipfs://${cid}`;
+  console.warn('Web3Storage removed. Use Supabase storage instead.');
+  return '';
 }
 
 export async function putJSON(obj, token, name = 'data.json') {
-  const blob = new Blob([JSON.stringify(obj)], { type: 'application/json' });
-  const file = new File([blob], name, { type: 'application/json' });
-  const client = getClient(token);
-  const cid = await client.put([file], { wrapWithDirectory: false });
-  return `ipfs://${cid}`;
+  console.warn('Web3Storage removed. Use Supabase storage instead.');
+  return '';
 }
